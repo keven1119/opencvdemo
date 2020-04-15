c5420c3a3e11eabea634dadb785a6c454e506262
kevencarben <liangzhenxing@carben.me>
2020-04-15 18:53:01 +0800

    first commit

--- opencvdemo/main.cpp
+++ opencvdemo/main.cpp
@@ -7,9 +7,54 @@
 //
 
 #include <iostream>
+#include <opencv2/opencv.hpp>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using namespace cv;
+
+int mfdain(int argc, const char * argv[]) {
+    
+    
+    
+    CascadeClassifier classifiter;
+    //加载模型
+    if(!classifiter.load("/Users/yons/Downloads/OpenCV-android-sdk/sdk/etc/lbpcascades/lbpcascade_frontalcatface.xml")){
+        
+        
+        return -1;
+        
+        
+    }
+    
+    Mat img = imread("/Users/yons/Downloads/many_preson.jpeg");
+    Mat gray;
+    
+
+    //降噪
+    cvtColor(img, gray, COLOR_BGR2GRAY);
+    
+//    printf("img piex size:%d\n", img.channels());
+//    printf("gray piex size:%d\n", gray.channels());
+
+//    imshow("原图", gray);
+
+    equalizeHist(gray, gray);
+
+//    imshow("灰度图", gray);
+    
+    std::vector<Rect> faces;
+    classifiter.detectMultiScale(gray, faces);
+        
+    for (Rect face:faces){
+        
+    
+        //画矩形
+        rectangle(img, face,Scalar(255, 0, 255));
+    }
+    
+    
+    imshow("a", img);
+
+    waitKey();
+    
     return 0;
 }
