c458a7976d425943094e83359a47df90f0ea8c2a
kevencarben <liangzhenxing@carben.me>
2020-04-15 12:25:50 +0800

    Initial Commit

---
+++ opencvdemo/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  opencvdemo
+//
+//  Created by yons on 15.4.20.
+//  Copyright © 2020 yons. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
